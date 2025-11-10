f4613459f0a1f25011b63d023061bcbed8f0151d
Azerchai <sinitsynad4@yandex.ru>
2025-11-10 15:22:22 +0300

    render create-palette with js

--- src/pages/createPalette.js
+++ src/pages/createPalette.js
@@ -1,24 +1,45 @@
-export function renderCreatePalette(outerElement){
-  outerElement.innerHTML = `<section>
-        <h2>Palette Editor</h2>
-        <div class="palette-editor">
-          <div class="palette-colors">
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch"></button>
-            <button class="color-swatch add-color-btn">+</button>
-          </div>
-          <div class="palette-properties">
+export function renderCreatePalette(outerElement, colors = []) {
 
-          </div>
-        </div>
-      </section>`;
+  const section = document.createElement('section');
+  outerElement.appendChild(section);
 
-    
+  const title = document.createElement('h2');
+  section.appendChild(title);
+  title.textContent = 'Palette Editor';
+
+  const editor = document.createElement('div');
+  section.appendChild(editor);
+  editor.classList.add('palette-editor');
+
+
+  const palette = document.createElement('div');
+  palette.classList.add('palette-colors');
+  editor.appendChild(palette);
+  
+  const properties = document.createElement('div');
+  properties.classList.add('palette-properties');
+  editor.appendChild(properties);
+
+  if (colors.length > 0) {
+    for (let c in colors) {
+      const swatch = document.createElement('button');
+      swatch.classList.add('color-swatch');
+      swatch.style.backgroundColor = c;
+      palette.appendChild(swatch);
+    }
+  }
+
+  else {
+    for (let i = 0; i < 6; i++) {
+      const swatch = document.createElement('button');
+      swatch.classList.add('color-swatch');
+      palette.appendChild(swatch);
+    }
+  }
+
+  const addBtn = document.createElement('button');
+      addBtn.classList.add('color-swatch', 'add-color-btn');
+      addBtn.textContent = '+';
+      palette.appendChild(addBtn);
+
 }
