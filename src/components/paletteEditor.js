b50d5a37041fdae198a3b8101d8a2a3ffb6679b0
Azerchai <sinitsynad4@yandex.ru>
2025-11-12 13:06:52 +0300

    added '+' button functionality in palette editor

--- src/components/paletteEditor.js
+++ src/components/paletteEditor.js
@@ -4,35 +4,45 @@ import { initColorInput } from "./colorInput.js";
 
 export function renderPaletteEditor(parent, colors) {
   const editor = createElement('div', parent, 'palette-editor');
-  
-    const palette = createElement('div', editor, 'palette-colors');
-    const properties = createElement('div', editor, 'palette-properties');
-  
-    const swatches = [];
-  
-    if (colors) {
-      for (let c of colors) {
-        const div = createElement('div', palette, 'color-swatch');
-        div.style.backgroundColor = c;
-        const colorInput = createElement('input', div, ['color-input'], { 'type': 'color' });
-        swatches.push(div);
-      }
-    }
-  
-    else {
-      for (let i = 0; i < 6; i++) {
-        const div = createElement('div', palette, 'color-swatch');
-        const colorInput = createElement('input', div, ['color-input'], { 'type': 'color' });
-        swatches.push(div);
-      }
-    }
-  
-    swatches.forEach((swatch) => {
-      initColorInput(swatch);
-    })
-  
-    const addBtn = createElement('button', palette, ['color-swatch', 'add-color-btn']);
-    addBtn.textContent = '+';
 
-    return editor;
+  const palette = createElement('div', editor, 'palette-colors');
+  const properties = createElement('div', editor, 'palette-properties');
+  properties.textContent = 'Lorem afkl;dskfllsdfldlfdjnjfngdjndgjnndf dfgdfgnjkdfngj sfdngfdjkg'
+
+  const swatches = [];
+
+  if (colors) {
+    for (let c of colors) {
+      appendColorSwatch(palette, c);
+    }
+  }
+
+  else {
+    for (let i = 0; i < 6; i++) {
+      appendColorSwatch(palette);
+    }
+  }
+
+  const addBtn = createElement('button', palette, ['color-swatch', 'add-color-btn']);
+  addBtn.textContent = '+';
+  addBtn.addEventListener('click', () => {
+    appendColorSwatch(palette);
+  })
+
+  return editor;
+}
+
+
+function appendColorSwatch(parent, color) {
+  const swatch = createElement('div', parent, 'color-swatch');
+  const colorInput = createElement('input', swatch, ['color-input'], { 'type': 'color' });
+  
+  const addBtn = parent.querySelector('.add-color-btn');
+  parent.insertBefore(swatch, addBtn);
+
+  initColorInput(swatch);
+
+  if (color) {
+    swatch.style.backgroundColor = color;
+  }
 }
